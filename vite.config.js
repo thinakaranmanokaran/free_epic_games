d105fec9840393dd0b52a161ebba98b9cfc47d60
Thinakaran <thinakaranmanohar@yahoo.com>
2025-09-03 20:01:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/free_epic_games/',   // 👈 Add this line
   plugins: [react()],
   server: {
     proxy: {
